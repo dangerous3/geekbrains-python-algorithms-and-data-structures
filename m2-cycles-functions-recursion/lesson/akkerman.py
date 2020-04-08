adcfdd9c22932f19790c7b38ddb7a020e0af2f8a
dangerous3 <dangerous3@yandex.ru>
2020-04-09 01:03:30 +0300

    Решение разобранных на видео задач из урока 2 "Циклы, рекурсия, функции" (рекурсия, функция Аккермана, алгоритм Евклида)

---
+++ m2-cycles-functions-recursion/lesson/akkerman.py
@@ -0,0 +1,19 @@
+'''
+Реализация функции Аккермана с помощью рекурсии
+'''
+
+import sys
+
+sys.setrecursionlimit(3000)
+
+def akk(m, n):
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return akk(m - 1, 1)
+    return akk(m - 1, akk(m, n - 1))
+
+print(akk(1, 1))
+print(akk(2, 4))
+print(akk(3, 4))
+print(akk(3, 8)) # sys.setrecursionlimit(3000)
