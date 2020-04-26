aa2d0cbfdf7fb2ba91e828a16b4e78f94f1693a5
dangerous3 <dangerous3@yandex.ru>
2020-04-26 14:48:39 +0300

    Реализация сортировки Шелла на Python

---
+++ m7-sorting/shell-sorting.py
@@ -0,0 +1,30 @@
+# сортировка Шелла (Дональд Шелл)
+
+import random
+
+# размер сортируемого массива
+size = 10
+array = [i for i in range(size)]
+random.shuffle(array)
+print(array)
+
+
+def shell_sort(array):
+    assert len(array) < 4000, 'Массив слишком большой. Используйте другой тип сортировки.'
+
+    def new_increment(array):
+        inc = [1, 4, 10, 23, 57, 132, 301, 701, 1750]
+        while len(array) <= inc[-1]:
+            inc.pop()
+        while len(inc) > 0:
+            yield inc.pop()
+
+    for increment in new_increment(array):
+        for i in range(increment, len(array)):
+            for j in range(i, increment - 1, -increment):
+                if array[j - increment] <= array[j]:
+                    break
+                array[j], array[j - increment] = array[j - increment], array[j]
+
+shell_sort(array)
+print(array)
